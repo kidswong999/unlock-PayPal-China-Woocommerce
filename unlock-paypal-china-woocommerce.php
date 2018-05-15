5118f0ab3fdd30eef9beead911714aa5f497b6c0
王开智 <kidswong999@163.com>
2018-05-15 10:54:14 +0800

    Update unlock-paypal-china-woocommerce.php

--- unlock-paypal-china-woocommerce.php
+++ unlock-paypal-china-woocommerce.php
@@ -1,7 +1,7 @@
 < ?php
 /*
 Plugin Name: unlock-PayPal-China-Woocommerce
-Description: 解锁人民币市PayPal不可用
+Description: 解锁人民币时PayPal不可用
 Author: kidswong999
 */
 add_filter( 'woocommerce_paypal_supported_currencies', 'enable_custom_currency' );
